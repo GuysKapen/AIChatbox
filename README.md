1eb94bd257bb73d9a4ebe50d4100e318620ba4bb
guys <guyskapen@gmail.com>
2022-09-09 14:21:27 -0400

    Init chatbox wiki questions

---
+++ README.md
@@ -0,0 +1,13 @@
+# Chatbox with AI functionalities
+
+## Answer questioning
+
+## Text summarize
+
+## Image styling
+
+## Image caption
+
+## Image generate
+
+## Music generate
---
+++ actions/actions.py
@@ -0,0 +1,48 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+# from typing import Any, Text, Dict, List
+#
+import spacy
+from typing import List, Dict, Text, Any
+
+from rasa_sdk import Action, Tracker
+from .components import QueryProcessor, DocumentRetrieval, PassageRetrieval, AnswerExtractor
+
+# from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionAnswerInfoQuestion(Action):
+    def name(self) -> Text:
+        return "action_answer_info_question"
+
+    # noinspection PyPep8Naming
+    async def run(self, dispatcher, tracker: Tracker, domain) -> List[Dict[Text, Any]]:
+        # SPACY_MODEL = os.environ.get('SPACY_MODEL', 'en_core_web_sm')
+        # QA_MODEL = os.environ.get('QA_MODEL', 'distilbert-base-cased-distilled-squad')
+        SPACY_MODEL = 'en_core_web_sm'
+        QA_MODEL = 'distilbert-base-cased-distilled-squad'
+        nlp = spacy.load(SPACY_MODEL, disable=['ner', 'parser', 'textcat'])
+        query_processor = QueryProcessor(nlp)
+        document_retriever = DocumentRetrieval()
+        passage_retriever = PassageRetrieval(nlp)
+        answer_extractor = AnswerExtractor(QA_MODEL, QA_MODEL)
+
+        question = tracker.latest_message.get('text')
+
+        query = query_processor.generate_query(question)
+        docs = document_retriever.search(query)
+        passage_retriever.fit(docs)
+        passages = passage_retriever.most_similar(question)
+        answers = answer_extractor.extract(question, passages)
+        if len(answers) == 0:
+            dispatcher.utter_message("Not found")
+        dispatcher.utter_message(
+                    text=answers[0].get('answer'))
+        return []
